cec60906814dd7c6b0aeed960fbcc9db723457e1
emily <emily.a.h@hotmail.com>
2020-12-22 10:22:34 -0700

    Tweaked orm, heroku troubleshooting

--- config/orm.js
+++ config/orm.js
@@ -26,9 +26,7 @@ function objToSql(ob) {
 // Database functions
 var orm = {
   showAll: function(table, cb) {
-    var query = "SELECT * FROM ?? ";
-
-    connection.query(query, table, function(err, result) {
+    connection.query("SELECT * FROM ??", table, function(err, result) {
       if (err) {
         throw err;
       }
